d73d8fc80e6f9c695e5887d8a47fd3e3b0db88f2
lxz <1291641211@qq.com>
2020-01-22 15:04:28 +0800

    前端-通讯录未对接版
    未使用服务器通讯录数据.

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-
-  //获取用户信息
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+    // 获取用户信息
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
@@ -27,22 +32,8 @@ App({
         }
       }
     })
-
-    console.log("app  launch!");
-    // 登录
-  
   },
   globalData: {
-    title: '标题',
-    userInfo: null,
-    cardvalue:['姓名'],
-    statusBarHeight: wx.getSystemInfoSync()['statusBarHeight'],
-    openid:'',
-    unionid:'',
-    serverUrl:'https://www.yatatek.xyz',
-    mdepMap: { 1: '文娱体育部', 2: '人资财务部', 3: '媒体宣传部', 4: '学术调研部', 5: '外招生部', 6: '旭日青年志愿者服务队', 7: '综合办公室', 8:'主席团'},
-    sdepMap: { 1: '司仪礼仪队', 2: '其他', 3: '编辑部', 4: '新媒体部', 5: '宣传部', 6: '旭日青年志愿者', 7: '心理服务部'},
-    mdepMap_R: { '文娱体育部':1, '人资财务部':2,  '媒体宣传部':3,  '学术调研部':4,  '外招生部':5,  '旭日青年志愿者服务队':6,  '综合办公室':7,  '主席团':8 },
-    sdepMap_R: {  '司仪礼仪队':1,  '其他':2,  '编辑部':3,  '新媒体部':4,  '宣传部':5,  '旭日青年志愿者':6,  '心理服务部':7 }
+    userInfo: null
   }
 })
